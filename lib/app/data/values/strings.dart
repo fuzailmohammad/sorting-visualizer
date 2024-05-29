410a2ac891efcbc81cb0d8cee8a8c6fb7f53ddd5
Mohammad Fuzail <30768724+fuzailmohammad@users.noreply.github.com>
2024-05-29 22:50:44 +0530

    initialize

---
+++ lib/app/data/values/strings.dart
@@ -0,0 +1,22 @@
+class Strings {
+  Strings._privateConstructor();
+
+  static const appName = 'Sample App';
+  static const empty = '';
+  static const mobileNumber = 'Mobile Number';
+  static const getOTP = 'Get OTP';
+  static const retry = 'Retry';
+}
+
+class ErrorMessages {
+  ErrorMessages._privateConstructor();
+
+  static const unauthorized = 'You are not authorized';
+  static const noInternet = 'Please check your internet connection';
+  static const connectionTimeout = 'Please check your internet connection';
+  static const networkGeneral = 'Something went wrong. Please try again later.';
+  static const invalidPhone = 'Invalid Mobile number';
+  static const invalidOTP = 'Invalid OTP';
+  static const invalidName = 'Invalid Name';
+  static const invalidEmail = 'Invalid Email';
+}
